daf9942aa26ac458dc22fd511ebfe5afe55337ee
herzog <thomas.herzog@gepardec.com>
2017-10-09 16:05:21 +0200

    - Copied build jobs to s2i jenkins directory

--- buildserver/openshift-buildserver.sh
+++ buildserver/openshift-buildserver.sh
@@ -4,34 +4,24 @@ SCRIPT_DIR=$(dirname ${0})
 JENKINS_S2I_ROOT=${SCRIPT_DIR}/jenkins-s2i
 
 function clear() {
+  oc login --username ${USERNAME} --password ${PASSWORD}
   oc delete project ${PROJECT_ID}
+  oc logout
 }
 
 function create() {
   oc login --username ${USERNAME} --password ${PASSWORD}
   oc new-project ${PROJECT_ID}
-  oc new-app -f jenkins-pipeline-example
+  oc new-app -f ${SCRIPT_DIR}/templates/hogarama-jenkins.yml -p "JENKINS_SERVICE_NAME=hogarama-jenkins"
+  oc logout
 }
 
 function restore() {
-  clear && create ${0} ${1}
+  clear && create
 }
 
-function build_s2i_images(){
-  # Build our own jenkins image, because we want to configure jenkins as we need it
-  s2i build \
-    --context-dir="${JENKINS_S2I_ROOT}" \
-    --loglevel=3 \
-    --pull-policy=never \
-    . \
-    openshift3/jenkins-2-rhel7 \
-    gepardec/jenkins-2-rhel7-hogarama
-
-  
-}
-
-PROJECT_ID=${PROJECT_ID:-"hogarama-buildserver"}
 USERNAME=${2:-"developer"}
 PASSWORD=${3:-"developer"}
+PROJECT_ID=${4:-"hogarama-buildserver"}
 
 ${1}
