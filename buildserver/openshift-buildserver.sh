8f2bf8064cb8c666ac435a51da0980a31aa41e39
herzog <thomas.herzog@gepardec.com>
2017-10-20 10:08:24 +0200

    - Deleted fluentd yml file and replaced it with template for creating all openshfit build configurations - Jenkins directory provides pipeline for building the jenkins image

--- buildserver/openshift-buildserver.sh
+++ buildserver/openshift-buildserver.sh
@@ -22,34 +22,51 @@ function create() {
 
   # Create build configs
   oc create -f templates/hogarama-image-streams.yml -n ${PROJECT_ID}
-  oc create -f templates/hogarama-fluentd.yml -n ${PROJECT_ID}
 
   # Create apps
+  # Build config creation for all Hogarama openshift builds
+  oc new-app -f templates/hogarama-build-configs.yml \
+    -p "JENKINS_GIT_REPO_URL=${GIT_URL}" \
+    -p "JENKINS_GIT_REPO_REF=${GIT_REF}" \
+    -p "FLUENTD_GIT_REPO_URL=${GIT_URL}" \
+    -p "FLUENTD_GIT_REPO_REF=${GIT_REF}"
+  # Nexus app which is the maven mirror
   oc new-app -f ${SCRIPT_DIR}/templates/hogarama-nexus.yml \
     -p "SERVICE_NAME=nexus"
+  # Pipeline for the Hogajama build
   oc new-app -f ${SCRIPT_DIR}/templates/hogarama-jenkins-pipeline-git.yml \
     -p "APP_NAME=hogajama" \
-    -p "GIT_REPO=https://github.com/Gepardec/Hogarama.git" \
-    -p "GIT_REF=OPENSHIFT_JENKINS_PIPELINE" \
+    -p "GIT_REPO=${GIT_URL}" \
+    -p "GIT_REF=${GIT_REF}" \
     -p "JENKINS_FILE_PATH=Hogajama/build/Jenkinsfile" \
     -p "MAVEN_MIRROR_URL=http://nexus:8081/repository/maven-public/"
+  # Pipeline for the mqtt-java-client build
   oc new-app -f ${SCRIPT_DIR}/templates/hogarama-jenkins-pipeline-git.yml \
     -p "APP_NAME=mqtt-java-lcient" \
-    -p "GIT_REPO=https://github.com/Gepardec/Hogarama.git" \
-    -p "GIT_REF=OPENSHIFT_JENKINS_PIPELINE" \
+    -p "GIT_REPO=${GIT_URL}" \
+    -p "GIT_REF=${GIT_REF}" \
     -p "JENKINS_FILE_PATH=mqtt-client-java/build/Jenkinsfile" \
     -p "MAVEN_MIRROR_URL=http://nexus:8081/repository/maven-public/"
+  # Pipeline for the Fluentd build
   oc new-app -f ${SCRIPT_DIR}/templates/hogarama-jenkins-pipeline-git.yml \
     -p "APP_NAME=fluentd" \
-    -p "GIT_REPO=https://github.com/Gepardec/Hogarama.git" \
-    -p "GIT_REF=OPENSHIFT_JENKINS_PIPELINE" \
+    -p "GIT_REPO=${GIT_URL}" \
+    -p "GIT_REF=${GIT_REF}" \
     -p "JENKINS_FILE_PATH=Fluentd/build/Jenkinsfile" \
     -p "MAVEN_MIRROR_URL=http://nexus:8081/repository/maven-public/"
+  # Pipeline for executing all builds parallel
   oc new-app -f ${SCRIPT_DIR}/templates/hogarama-jenkins-pipeline-git.yml \
     -p "APP_NAME=all-parallel" \
-    -p "GIT_REPO=https://github.com/Gepardec/Hogarama.git" \
-    -p "GIT_REF=OPENSHIFT_JENKINS_PIPELINE" \
+    -p "GIT_REPO=${GIT_URL}" \
+    -p "GIT_REF=${GIT_REF}" \
     -p "JENKINS_FILE_PATH=buildserver/Jenkinsfile" \
+    -p "MAVEN_MIRROR_URL=http://nexus:8081/repository/maven-public/"
+  # Pipeline for jenkins build
+  oc new-app -f ${SCRIPT_DIR}/templates/hogarama-jenkins-pipeline-git.yml \
+    -p "APP_NAME=jenkins" \
+    -p "GIT_REPO=${GIT_URL}" \
+    -p "GIT_REF=${GIT_REF}" \
+    -p "JENKINS_FILE_PATH=Jenkins/build/Jenkinsfile" \
     -p "MAVEN_MIRROR_URL=http://nexus:8081/repository/maven-public/"
 
   oc logout
@@ -57,4 +74,7 @@ function create() {
 
 PROJECT_ID=${2:-"hogarama-buildserver"}
 NAMESPACE='gepardec'
+GIT_URL="https://github.com/Gepardec/Hogarama.git"
+GIT_REF="OPENSHIFT_JENKINS_PIPELINE"
+
 ${1}
